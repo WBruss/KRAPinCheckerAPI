0e09c0c5c0b9efd47e926074aba29f8ba74cdd09
wbruss <kihoropaul@gmail.com>
2021-10-23 07:02:03 +0300

    Attempt with mechanize Extracted form

--- src/pin_checker.py
+++ src/pin_checker.py
@@ -2,5 +2,18 @@ import mechanize
 
 
 def checkPin():
-    return "Hello Pin"
+    br = mechanize.Browser()
+    response = br.open("https://itax.kra.go.ke/KRA-Portal/pinChecker.htm")
+    print(response.read())
+
+    for form in br.forms():
+        print("Form name: ", form.name)
+        print(form)
+
+        print("Controls")
+
+        for control in form.controls:
+            print(control)
+            print("type=%s, name=%s", control.type, control.name)
+
 
